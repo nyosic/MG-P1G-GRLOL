5b1af4f7e09d86ff5d15a2675d208bf0a8f30924
szty-v <lookgrit1@gmail.com>
2024-11-26 17:00:54 +0100

    added fixes to javascript and css (menu)

--- jscript/scriptmenu.js
+++ jscript/scriptmenu.js
@@ -27,19 +27,23 @@ tabs.forEach((tab, index) => {
 
 function showSection(sectionId) {
     const sections = document.querySelectorAll('.menu-section');
+
     sections.forEach(section => {
-        section.style.display = 'none';
+        if (section.id === sectionId) {
+            section.classList.remove('hide'); 
+            section.classList.add('show');  
+        } else {
+            section.classList.remove('show'); 
+            section.classList.add('hide');  
+        }
     });
 
-    const activeSection = document.getElementById(sectionId);
-    if (activeSection) {
-        activeSection.style.display = 'block';
-    }
 
     const tabs = document.querySelectorAll('.menu-tabs .tab');
     tabs.forEach(tab => tab.classList.remove('active'));
     document.querySelector(`.tab[onclick="showSection('${sectionId}')"]`).classList.add('active');
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
     showSection('brunch');
